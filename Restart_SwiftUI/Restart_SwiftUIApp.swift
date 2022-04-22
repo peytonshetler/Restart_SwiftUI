1c71282788f1b3c12398b8e71c74167f5e665524
Peyton Shetler <peyton.shetler@gmail.com>
2022-04-22 14:13:04 -0500

    Initial Commit

---
+++ Restart_SwiftUI/Restart_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restart_SwiftUIApp.swift
+//  Restart_SwiftUI
+//
+//  Created by Peyton Shetler on 4/22/22.
+//
+
+import SwiftUI
+
+@main
+struct Restart_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
